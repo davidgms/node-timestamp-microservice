541fc4c4e067fcac80a6cf2702d68d0e1df6e16e
David Gomes de Carvalho <davidgms31@gmail.com>
2025-07-15 17:49:12 -0300

    new node project

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-// index.js
-// where your node app starts
+// where node app starts
 
 // init project
 var express = require('express');
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing) so that the API is remotely testable by FCC 
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -19,11 +17,26 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+app.get("/api/:date?", (req, res) => {
+  const dateParam = req.params.date;
+  let date;
 
+  if (!dateParam) {
+    date = new Date();
+  } else {
+    if (/^\d+$/.test(dateParam)) {
+      date = new Date(parseInt(dateParam, 10));
+    } else {
+      date = new Date(dateParam);
+    }
+  }
+
+  if (isNaN(date.getTime())) {
+    res.json({ error: "Invalid Date" });
+  } else {
+    res.json({ unix: date.getTime(), utc: date.toUTCString() });
+  }
+});
 
 
 // Listen on port set in environment variable or default to 3000
